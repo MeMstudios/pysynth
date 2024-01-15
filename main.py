c89c786d9d5c62ab5784f7bd48f00bd715eab6f6
MeMstudios <michaelmurphystudios@gmail.com>
2024-01-15 16:04:50 -0500

    update pygame loop.  add display

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ from synth import MonoSynth
 import pygame
 from pygame.locals import KEYDOWN, KEYUP, K_ESCAPE, K_RIGHT, K_UP, K_DOWN, K_LEFT
 
-if __name__ == "__main__":
-    synth = MonoSynth(sample_rate=44100)
-    synth.shape = "square"
-    pygame.init()
-    while True:
-        event = pygame.event.wait()
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                break
-            elif event.key == K_RIGHT:
-                print("Transposing up!")
-                synth.transpose_up()
-            elif event.key == K_UP:
-                print("Octave up!")
-                synth.octave_up()
-            elif event.key == K_DOWN:
-                print("Octave down!")
-                synth.octave_down()
-            else:
-                synth.handle_key_press(event.key)
-        elif event.type == KEYUP:
-            synth.handle_key_release(event.key)
+synth = MonoSynth(sample_rate=44100)
+synth.shape = "square"
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+running = True
+print("Welcome to PySynth!")
+
+while running:
+    event = pygame.event.wait()
+    if event.type == KEYDOWN or event.type == pygame.QUIT:
+        if event.key == K_ESCAPE:
+            break
+        elif event.key == K_RIGHT:
+            print("Transposing up!")
+            synth.transpose_up()
+        elif event.key == K_UP:
+            print("Octave up!")
+            synth.octave_up()
+        elif event.key == K_DOWN:
+            print("Octave down!")
+            synth.octave_down()
+        else:
+            synth.handle_music_key_press(event.key)
+    elif event.type == KEYUP:
+        synth.handle_music_key_release(event.key)
