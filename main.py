8e7f068fa0fa62f6283f61e4cba8b9beb6d5558c
MeMstudios <michaelmurphystudios@gmail.com>
2024-01-16 12:30:13 -0500

    add wave display

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from synth import MonoSynth
 import pygame
 from pygame.locals import KEYDOWN, KEYUP, K_ESCAPE, K_RIGHT, K_UP, K_DOWN, K_LEFT
+import math
 
-synth = MonoSynth(sample_rate=44100)
-synth.shape = "square"
+hz, sample_rate = 440, 44100
+synth = MonoSynth(hz, sample_rate)
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
+width, height = 1280, 720
+screen = pygame.display.set_mode((width, height))
+
+clock = pygame.time.Clock()
+
+x_offset = 0
 running = True
+freq = 0
+
 print("Welcome to PySynth!")
 
 while running:
-    #event = pygame.event.wait()
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
@@ -24,8 +32,28 @@ while running:
                 print("Octave down!")
                 synth.octave_down()
             else:
-                synth.handle_music_key_press(event.key)
+                freq = synth.handle_music_key_press(event.key)
         elif event.type == KEYUP:
             synth.handle_music_key_release(event.key)
+            freq = 0
         elif event.type == pygame.QUIT:
             running = False
+
+    if not freq:
+        freq = 0
+
+    screen.fill("purple")
+    # Draw the wave
+    points = []
+
+    for x in range(0, width):
+        y = int(100 * math.sin(freq * (x + x_offset)))
+        points.append((x, height // 2 + y))
+
+    pygame.draw.lines(screen, (0, 0, 0), False, points, 2)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+    x_offset += sample_rate
